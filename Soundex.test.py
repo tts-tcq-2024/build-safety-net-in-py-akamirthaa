0a18118c4fd7bc1cb1479446c5a28a2b5e14090b
akamirthaa <ak.amirthaa@gmail.com>
2024-09-22 23:59:22 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -15,8 +15,15 @@ class TestSoundex(unittest.TestCase):
     def test_lowercase(self):
         self.assertEqual(generate_soundex("a"),"A000")
 
-    def test_numbers(self):
+    def test_numbers_and_characters(self):
         self.assertEqual(generate_soundex("James123"), "J520")
+    
+    def test_only_numbers(self):
+        self.assertEqual(generate_soundex("12345"),"1000")
+
+    def test_similar_words(self):
+        self.assertEqual(generate_soundex("chebyshev"), "C121")
+        self.assertEqual(generate_soundex("tchebycheff"), "T121")
     
 if __name__ == '__main__':
     unittest.main()
