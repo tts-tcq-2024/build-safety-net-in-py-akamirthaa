a2af813a32596e3d40c22d5d88ae0a19cc517279
akamirthaa <ak.amirthaa@gmail.com>
2024-09-23 01:34:24 +0530

    adding new testcases

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,29 +1,38 @@
 import unittest
-from Soundex import generate_soundex
+from Soundex import generate_final_soundex
+from Soundex import add_zeros
+from Soundex import four_characters
+from Soundex import initial_soundex
+from Soundex import drop_letters
+from Soundex import get_first_letter
 
 class TestSoundex(unittest.TestCase):
-
-    def test_empty_string(self):
-        self.assertEqual(generate_soundex(""), "")
-
-    def test_single_character(self):
-        self.assertEqual(generate_soundex("A"), "A000")
-
-    def test_repeating_character(self):
-        self.assertEqual(generate_soundex("Hermann"),"H655")
-
-    def test_lowercase(self):
-        self.assertEqual(generate_soundex("a"),"A000")
-
-    def test_numbers_and_characters(self):
-        self.assertEqual(generate_soundex("James123"), "J520")
+    def test_string_format(self):
+        self.assertEqual(generate_final_soundex(""), "No Characters found")
+        self.assertEqual(generate_final_soundex("1234"),"No Characters found")
     
-    def test_only_numbers(self):
-        self.assertEqual(generate_soundex("12345"),"1000")
+    def test_first_letter(self):
+        self.assertEqual(get_first_letter("tchebycheff"), "T")
+        self.assertEqual(get_first_letter("TCHEBYCHEFF"), "T")
+
+    def test_drop_letters(self):
+        self.assertEqual(drop_letters("tchebycheff"),"tcbcff" )
+    
+    def test_initial_soundex(self):
+        self.assertEqual(initial_soundex("tchebycheff"),"T21211")
+
+    def test_four_characters(self):
+        self.assertEqual(four_characters("T21211"),"T212")
+        self.assertEqual(four_characters("T12"),"T12")
+    
+    def test_add_zeros(self):
+        self.assertEqual(add_zeros("T212"),"T212")
+        self.assertEqual(add_zeros("T2"),"T200")
 
     def test_similar_words(self):
-        self.assertEqual(generate_soundex("chebyshev"), "C121")
-        self.assertEqual(generate_soundex("tchebycheff"), "T121")
+        self.assertEqual(generate_final_soundex("Robert"),'R163')
+        self.assertEqual(generate_final_soundex("Rupert"),"R163")
+
     
 if __name__ == '__main__':
     unittest.main()
