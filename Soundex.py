a149806783594782c218b0332b19a83348ccf4ab
akamirthaa <ak.amirthaa@gmail.com>
2024-09-22 21:26:11 +0530

    Update Soundex.py - remove prev-code idea

--- Soundex.py
+++ Soundex.py
@@ -17,13 +17,11 @@ def generate_soundex(name):
 
     # Start with the first letter (capitalized).
     soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
 
     for char in name[1:]:
         code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
+        if code != '0' and code != soundex[:-1]:
             soundex += code
-            prev_code = code
         if len(soundex) == 4:
             break
 
