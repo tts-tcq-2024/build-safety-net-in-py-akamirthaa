de777e4506aac2e34e689da51b87450ea6b7a674
akamirthaa <ak.amirthaa@gmail.com>
2024-09-29 18:48:35 +0530

    Update Soundex.py - less functions

--- Soundex.py
+++ Soundex.py
@@ -1,4 +1,5 @@
 def get_soundex_code(c):
+    c = c.upper()
     mapping = {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
@@ -7,47 +8,21 @@ def get_soundex_code(c):
         'M': '5', 'N': '5',
         'R': '6'
     }
-    return mapping.get(c.upper(),'0')  # Default to '0' for non-mapped characters
-
-    
-# Start with the first letter (capitalized).
-def get_first_letter(name):
+    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
+def generate_soundex(name):
+    if not name:
+        return ""
+    # Start with the first letter (capitalized)
     soundex = name[0].upper()
-    return (soundex)
-
-# Drop all occurrences of a, e, i, o, u, y, h, w
-def drop_letters(name):
-    list_of_letters_to_drop = ['a', 'e', 'i', 'o', 'u', 'y', 'h', 'w']
-    for letter_to_drop in name[1:]:
-        if letter_to_drop in list_of_letters_to_drop:
-            name = name.replace(letter_to_drop,"")
-    return name
-
-
-def initial_soundex(name, soundex):
     for letter in name[1:]:
         code_for_letter = get_soundex_code(letter)
-        if code_for_letter != soundex[:-1]:
+        if code_for_letter != 0 and code_for_letter != soundex[-1]:
             soundex += code_for_letter
-    return soundex
-
-def four_characters(soundex):
-    soundex = soundex[:4]
-    return soundex
-
-# Pad with zeros if necessary
+            if len(soundex) == 4:
+                break
+    return add_zeros(soundex)
+    
 def add_zeros(soundex):
+    # Pad with zeros if necessary
     soundex = soundex.ljust(4, '0')
     return soundex
-
-def generate_final_soundex(name):
-    if name == "":
-        return ""
-    if name.isnumeric():
-        return ""
-    first_letter = get_first_letter(name)
-    name_after_dropping_letters = drop_letters(name)
-    initial_soundex_code = initial_soundex(name_after_dropping_letters, first_letter)
-    four_char_soundex_code = four_characters (initial_soundex_code)
-    zero_added_soundex_code = add_zeros(four_char_soundex_code)
-    return zero_added_soundex_code
